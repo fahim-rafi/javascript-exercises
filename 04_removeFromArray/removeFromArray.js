3ace2df2c43ea9d0daf67888fe39369c99d6dc16
fahim-rafi <fahimrafi37@gmail.com>
2022-06-25 23:01:15 +0600

    upto 05 compelted

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+var removeFromArray = function (array, ...args) {
+  let newArray = array.filter((values) => !args.includes(values));
+  return newArray;
 };
 
 // Do not edit below this line
