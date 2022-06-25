0941998e19693049dee6019485b78633a1666abc
fahim-rafi <fahimrafi37@gmail.com>
2022-06-26 00:47:16 +0600

    completed till 07

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,16 @@
-const sumAll = function () {};
+const sumAll = function (a, b) {
+  //   var sum = 0;
+  //   for (let num = a; num <= b; num++) {
+  //     // Runs 5 times, with values of step 0 through 4.
+  //     sum = sum + num;
+  //   }
+  //   return sum;
+  if (Math.sign(a) == -1 || Math.sign(b) == -1) return "ERROR";
+  if (isNaN(a) || isNaN(b)) return "ERROR";
+  let n = Math.abs(a - b) + 1;
+  let sum = (n / 2) * (a + b);
+  return sum;
+};
 
 // Do not edit below this line
 module.exports = sumAll;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function (x) {
+  return Math.round((((x - 32.0) * 5.0) / 9.0) * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function (x) {
+  return Math.round(((x * 9.0) / 5.0 + 32.0) * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
